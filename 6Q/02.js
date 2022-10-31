14b19d4a0437398760c02d99cd67869788f919a7
talismanchik <talismanchik.by@gmail.com>
2022-10-31 14:53:15 +0300

    add Q6-02 Task

---
+++ 6Q/02.js
@@ -0,0 +1,23 @@
+function solution(s, t) {
+    let bestDistance = 0;
+    let firstArr = [];
+    let lastArr = [];
+    for (let i = 0; i < t; i++)  firstArr.push("R");
+    for (let i = 0; i < t; i++)  lastArr.push("S");
+for (let i = 0; i < t; i++){
+
+}
+
+
+
+
+
+
+
+
+    console.log(firstArr)
+    console.log(lastArr);
+    return bestDistance;
+}
+
+solution(2, 4)
