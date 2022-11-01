2e37aeba39ef2e81b197a3226f8b24650ffda8e1
talismanchik <talismanchik.by@gmail.com>
2022-11-01 11:42:24 +0300

    add Q8-24 Task

---
+++ 8Q/24.js
@@ -0,0 +1,11 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let dif = dadYearsOld - sonYearsOld;
+if (dif - sonYearsOld < 0){
+return sonYearsOld - dif;
+}else{
+    return dif - sonYearsOld;
+}
+}
+
+// 18 9 + 8
+// 26 17
